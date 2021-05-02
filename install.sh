41ffc3b33f3f17d8540a8db8d9e5934126eed8c5
vamsijakkula <krishnavamsi@gmail.com>
2021-05-02 11:40:26 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+wget https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv4.1.2/kustomize_v4.1.2_linux_amd64.tar.gz
+tar -xvzf kustomize_kustomize.v4.1.2_linux_amd64 .tar.gz
+chmod a+x kustomize
+sudo mv kustomize /usr/local/bin
